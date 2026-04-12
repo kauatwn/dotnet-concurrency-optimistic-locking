0e2683af20db4ddf36bca6390dbca8db8f3fcf06
kauatwn <kaualopescontato@gmail.com>
2026-04-12 16:44:03 -0300

    refactor: remove GetShowDetails query and related components

--- src/TicketFlow.Application/UseCases/Queries/Shows/GetShowDetails/IShowReadRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TicketFlow.Application.UseCases.Queries.Shows.GetShowDetails;
-
-public interface IShowReadRepository
-{
-    Task<ShowDetailsResponse?> GetDetailsAsync(Guid showId, CancellationToken cancellationToken);
-}
--- src/TicketFlow.Application/UseCases/Queries/Shows/GetShowDetails/ShowDetailsResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TicketFlow.Application.UseCases.Queries.Shows.GetShowDetails;
-
-public record ShowDetailsResponse(
-    Guid Id,
-    string Title,
-    DateTime Date,
-    string Status,
-    int TotalTickets,
-    int AvailableTickets);
