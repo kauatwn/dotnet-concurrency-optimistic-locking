2c1ea0e98b4cece13a5a5796fb8933b96f8ee0d5
kauatwn <kaualopescontato@gmail.com>
2026-04-12 16:29:39 -0300

    refactor: update database provider and clean up configurations

--- src/TicketFlow.Infrastructure/Extensions/DependencyInjectionExtensions.cs
+++ src/TicketFlow.Infrastructure/Extensions/DependencyInjectionExtensions.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics.CodeAnalysis;
-using TicketFlow.Application.UseCases.Queries.Shows.GetShowDetails;
-using TicketFlow.Application.UseCases.Queries.Tickets.GetAvailable;
+using TicketFlow.Application.UseCases.Shows.GetDetails;
 using TicketFlow.Domain.Repositories;
 using TicketFlow.Infrastructure.Persistence;
 using TicketFlow.Infrastructure.Persistence.Repositories;
@@ -27,24 +26,20 @@ public static class DependencyInjectionExtensions
     {
         services.AddDbContext<TicketFlowDbContext>(options =>
         {
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"), sqlOptions =>
             {
                 sqlOptions.EnableRetryOnFailure(
-                maxRetryCount: 5,
-                maxRetryDelay: TimeSpan.FromSeconds(10),
-                errorNumbersToAdd: null);
-            });
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(10),
+                    errorCodesToAdd: null);
+            }).UseSnakeCaseNamingConvention();
 
             options.EnableSensitiveDataLogging();
             options.LogTo(Console.WriteLine, LogLevel.Information);
         });
 
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
-
         services.AddScoped<ITicketRepository, TicketRepository>();
-        services.AddScoped<ITicketReadRepository, TicketReadRepository>();
-
         services.AddScoped<IShowRepository, ShowRepository>();
-        services.AddScoped<IShowReadRepository, ShowReadRepository>();
+        services.AddScoped<IShowReadRepository, ShowRepository>();
     }
 }
